be95e4c6f5a5c7d74a6f8a5f5425968dcb370a2c
POPORION <black4roach@gmail.com>
2023-10-03 16:21:16 +0900

    add manifests for datadog

--- terraform/dev/modules/eks/main.tf
+++ terraform/dev/modules/eks/main.tf
@@ -76,7 +76,7 @@ module "eks_blueprints_addons" {
     }
 
     enable_aws_load_balancer_controller    = true
-    enable_kube_prometheus_stack           = true
+    enable_kube_prometheus_stack           = false
     enable_metrics_server                  = true
     enable_external_dns                    = true
     enable_argocd                          = true
